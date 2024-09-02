adc788d1f550c88c1673649efb42eb5b7d6a661d
H-Ryuk <hassan01saadaoui@gmail.com>
2024-09-02 22:54:10 +0100

    add user repo

---
+++ src/main/java/com/hassan/pets/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package com.hassan.pets.repository;
+
+import com.hassan.pets.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+}
