6e365d63867faab03957576f59ae03fdbf4c5345
H-Ryuk <hassan01saadaoui@gmail.com>
2024-09-08 19:06:15 +0100

    Update Records Package

--- src/main/java/com/hassan/pets/Records/ItemRecord.java
+++ src/main/java/com/hassan/pets/Records/ItemRecord.java
@@ -1,7 +1,28 @@
-package com.hassan.pets.DTO;
+package com.hassan.pets.Records;
+
+import com.hassan.pets.Model.Categories;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public record ItemRecord( Long itemId, String itemName, BigDecimal price, int stock,
-                          byte[] itemImage, String categoryName, String categoryDescription) {
+public record ItemRecord
+        (Long itemId,
+
+         @NotBlank(message = "item name shouldn't be null")
+         String name,
+
+         @NotNull(message = "item price shouldn't be null")
+         BigDecimal price,
+
+         @NotNull(message = "item description shouldn't be null")
+         String description,
+
+         @NotNull(message = "stock of items shouldn't be null")
+         int stock,
+
+         byte[] imageUrl,
+
+         @NotNull(message = "every item should have one category")
+         Categories category) {
 }
--- src/main/java/com/hassan/pets/Records/UserRecord.java
+++ src/main/java/com/hassan/pets/Records/UserRecord.java
@@ -1,10 +1,31 @@
-package com.hassan.pets.DTO;
+package com.hassan.pets.Records;
 
 
+import com.hassan.pets.Model.Users;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record UserRecord
-        (Long userId, String username,
-         String password, String email,
-         String address, String phoneNumber) {
+        (Long userId,
+
+         @NotBlank(message = "Username shouldn't be null")
+         String username,
+
+         @NotBlank(message = "Password shouldn't be null")
+         String password,
+
+         @Email(message = "Invalid email address")
+         String email,
+
+         @NotNull(message = "Address shouldn't be null")
+         String address,
+
+         @Pattern(regexp = "^\\d{10}$", message = "Invalid mobile number entered")
+         String phoneNumber,
+
+         @NotNull(message = "Role shouldn't be null")
+         Users.UserRole role ) {
 }
 
