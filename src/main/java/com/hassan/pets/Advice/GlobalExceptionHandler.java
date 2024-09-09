9b2eacbcf0a074c8dd3afa03fffa22eddc6390c9
H-Ryuk <hassan01saadaoui@gmail.com>
2024-09-09 18:13:22 +0100

    Update Advice Global Exception Handler

---
+++ src/main/java/com/hassan/pets/Advice/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.hassan.pets.Advice;
+
+
+import com.hassan.pets.Exception.EmptyFieldException;
+import com.hassan.pets.Exception.RemovingItemFailedException;
+import com.hassan.pets.Exception.TargetNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+
+
+    @ExceptionHandler(TargetNotFoundException.class)
+    public ResponseEntity<String> handleTargetNotFoundException(TargetNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+
+    @ExceptionHandler(EmptyFieldException.class)
+    public ResponseEntity<String> handleEmptyFieldException(EmptyFieldException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(RemovingItemFailedException.class)
+    public ResponseEntity<String> handleRemovingItemFromCart(RemovingItemFailedException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+
+}
