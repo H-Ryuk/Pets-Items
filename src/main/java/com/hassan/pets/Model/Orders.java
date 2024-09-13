2b5197c51c29f0ce6f3531b41a4e8f19ceaae148
H-Ryuk <hassan01saadaoui@gmail.com>
2024-09-13 15:26:59 +0100

    Fix Orders model

---
+++ src/main/java/com/hassan/pets/Model/Orders.java
@@ -0,0 +1,70 @@
+package com.hassan.pets.Model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Component
+@Scope("prototype")
+public class Orders implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm")
+    private LocalDateTime orderDate;
+
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+
+    @PrePersist
+    public void onCreate() {
+        this.orderDate = LocalDateTime.now();
+    }
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_fk")
+    private Users users;
+
+
+    public Orders(Long orderId, LocalDateTime orderDate, BigDecimal totalAmount, OrderStatus status, Users users) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+        this.status = status;
+        this.users = users;
+    }
+
+
+
+    public enum OrderStatus {
+        PENDING, DELIVERED, CANCELLED;
+    }
+
+
+}
+
