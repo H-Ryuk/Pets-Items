56aa306517697a6d1dd131970084d7215145188c
H-Ryuk <hassan01saadaoui@gmail.com>
2024-09-16 17:30:10 +0100

    Add User Repo

--- src/main/java/com/hassan/pets/Repository/UserRepo.java
+++ src/main/java/com/hassan/pets/Repository/UserRepo.java
@@ -2,6 +2,7 @@ package com.hassan.pets.Repository;
 
 import com.hassan.pets.Model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,8 @@ public interface UserRepo extends JpaRepository<Users,Long> {
 
     @Query("select u from Users u where email like :userEmail ")
     Optional<Users> findByEmail(@Param("userEmail") String userEmail);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "delete from users where user_id = :userId", nativeQuery = true)
+    int deleteUserById(Long userId);
 }
